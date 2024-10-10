4a275a0a015e11b4c090fd5820d9b107594aa722
sahira0205 <156178749+sahira0205@users.noreply.github.com>
2024-10-10 09:17:33 +0530

    Asynchronous,Synchronous,SetTimeout,SetInterval

---
+++ Class Work/Lec-32/script.js
@@ -0,0 +1,86 @@
+//Asynchronous
+// console.log("Started Execution");
+// function myfunc() {
+//     console.log("This is a function");
+// }
+// myfunc();
+
+// console.log("Execution Ends Here..");
+
+
+
+//callback
+// function myfunc(){
+//     console.log("My Function is");
+// }
+// function hello(call){
+//     console.log("THis is hello ");
+//     call();
+// }
+// hello(myfunc); //hello func in myfunc
+
+
+
+//Synchronous
+// console.log("Execution start..");
+// for(let i=0;i<1000;i++){
+//     console.log("For loop running");
+// }
+// console.log("Execution ends..");
+//js is asynchronus lang but its execution is synchronus;
+
+
+
+//set timeout: this func execute after a specified delay.
+// function setTimeout(){
+// }
+
+// var set = function setTimeout(){
+// }
+
+// setTimeout(()=>{
+//     console.log("SetTimeout Function");
+// },2000);
+// function first(){
+//     console.log('This is a fist function');
+// }
+// first();
+
+// console.log ("Execution started");
+// setTimeout(()=>{
+//  console.log("SetTimeout function");
+// },2000);
+// for(let i=0;i<=1000;i++){
+//     console.log("Loop is running");
+// }
+// console.log("Execution ends");
+
+
+// console.log("start");
+// var a= setTimeout(()=>{
+//     console.log("SetTimeout working");
+// },1000);
+// clearTimeout(a);
+// console.log("ends");
+
+// console.log("start");
+// var a= setTimeout(()=>{
+//     console.log("SetTimeout working");
+// },1000);
+// clearInterval(a);
+// console.log("ends");
+
+//let count=0;
+// const interval=setInterval(()=>{
+//     count++
+//     console.log(
+//         `Message: ${count}`
+//     );
+//     if(count>=3){
+//         clearInterval(interval);
+//         console.log("Ends");
+//     }
+// });
+
+
+
